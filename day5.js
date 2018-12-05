93f598acf82b40c7fbea2e7aa65fa4ea45875557
Adam Shumann <ashumann13@gmail.com>
2018-12-04 23:26:46 -0600

    Day5

---
+++ day5.js
@@ -0,0 +1,33 @@
+const moment = require('moment')
+
+const argv = process.argv.slice(2)
+const input = require('./input')
+
+let reacted = true
+const regexParts = []
+for(let i=65;i<65+26;i++) {
+  const a = String.fromCharCode(i)
+  const b = a.toLowerCase()
+  regexParts.push(a+b,b+a)
+}
+const regex = new RegExp(regexParts.join('|'),'g')
+console.log('part1',react(input[0]))
+
+const results = []
+for(let i=65;i<65+26;i++) {
+  const a = String.fromCharCode(i)
+  const b = a.toLowerCase()
+  const result = react(input[0].replace(new RegExp(a,'ig'), ''))
+  results.push(result)
+}
+const min = results.reduce((a,b) => Math.min(a, b), 1e100)
+console.log('part2', min)
+
+function react(res) {
+  while(true) {
+    let n = res.replace(regex, '')
+    if (n === res) break
+    res = n
+  }
+  return res.length
+}
