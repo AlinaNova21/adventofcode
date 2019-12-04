e4b362c7e86f5d7c2852eafac6731f12f25084c0
Adam Shumann <ashumann13@gmail.com>
2019-12-03 23:27:01 -0600

    Day4

--- 2019/day04/day.go
+++ 2019/day04/day.go
@@ -1,10 +1,46 @@
 package day04
 
-import "github.com/ags131/adventofcode/2019/aoc"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/ags131/adventofcode/2019/aoc"
+)
 
 // Run runs this day
 func Run(input *aoc.Input) aoc.Output {
-	part1 := "Not Implemented"
-	part2 := "Not Implemented"
+	var start, end int
+	fmt.Fscanf(input, "%d-%d", &start, &end)
+	part1 := 0
+	part2 := 0
+	for n := start; n <= end; n++ {
+		str := strconv.Itoa(n)
+		if str[0] <= str[1] && str[1] <= str[2] && str[2] <= str[3] && str[3] <= str[4] && str[4] <= str[5] {
+			cnt := map[rune]int{}
+			for _, c := range str {
+				if v, ok := cnt[c]; ok {
+					cnt[c] = v + 1
+				} else {
+					cnt[c] = 1
+				}
+			}
+			hasTwo := false
+			hasMore := false
+			for _, n := range cnt {
+				if n == 2 {
+					hasTwo = true
+				}
+				if n >= 2 {
+					hasMore = true
+				}
+			}
+			if hasMore {
+				part1++
+				if hasTwo {
+					part2++
+				}
+			}
+		}
+	}
 	return aoc.Output{Part1: part1, Part2: part2}
 }
