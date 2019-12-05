90c2c684efc8bd49e83ed37539024ef26be84344
Adam Shumann <ashumann13@gmail.com>
2019-12-05 08:43:35 -0600

    Day5

--- 2019/aoc/intcode/intcode.go
+++ 2019/aoc/intcode/intcode.go
@@ -1,5 +1,10 @@
 package intcode
 
+import (
+	"fmt"
+	"math"
+)
+
 // OPCode determines the operation to execute
 type OPCode int
 
@@ -7,13 +12,32 @@ const (
 	OPCodeNop OPCode = iota
 	OPCodeAdd
 	OPCodeMul
+	OPCodeInput
+	OPCodeOutput
+	OPCodeJumpIfTrue
+	OPCodeJumpIfFalse
+	OPCodeLessThan
+	OPCodeEquals
 	OPCodeHalt OPCode = 99
 )
+
+// ParameterMode changes how parameters are read
+type ParameterMode int
+
+const (
+	ParameterModePosition = iota
+	ParameterModeImmediate
+)
+
+type InputFunc func() int
+type OutputFunc func(v int)
 
 // Machine is an intcode interpreter
 type Machine struct {
 	IP     int
 	Memory []int
+	Input  InputFunc
+	Output OutputFunc
 }
 
 // Run executes until halt
@@ -22,17 +46,80 @@ func (m *Machine) Run() {
 	}
 }
 
+func (m *Machine) Get(addr int) int {
+	return m.Memory[addr]
+}
+
+func (m *Machine) Set(addr, v int) {
+	m.Memory[addr] = v
+}
+
+func (m *Machine) getParam(off int) int {
+	v := m.Memory[m.IP]
+	t1 := int(math.Pow(10, float64(off+2)))
+	t2 := int(math.Pow(10, float64(off+1)))
+	pm := ParameterMode((v % t1) / t2)
+	addr := m.IP + off
+	switch pm {
+	case ParameterModePosition:
+		return m.Memory[m.Memory[addr]]
+	case ParameterModeImmediate:
+		return m.Memory[addr]
+	}
+	return 0
+}
+
+func (m *Machine) setResult(off, v int) {
+	addr := m.IP + off
+	m.Memory[m.Memory[addr]] = v
+}
+
 // Step executes one instruction
 func (m *Machine) Step() bool {
-	switch OPCode(m.Memory[m.IP]) {
+	op := OPCode(m.Memory[m.IP] % 100)
+	switch op {
 	case OPCodeAdd:
-		m.Memory[m.Memory[m.IP+3]] = m.Memory[m.Memory[m.IP+1]] + m.Memory[m.Memory[m.IP+2]]
+		m.setResult(3, m.getParam(1)+m.getParam(2))
 		m.IP += 4
 	case OPCodeMul:
-		m.Memory[m.Memory[m.IP+3]] = m.Memory[m.Memory[m.IP+1]] * m.Memory[m.Memory[m.IP+2]]
+		m.setResult(3, m.getParam(1)*m.getParam(2))
+		m.IP += 4
+	case OPCodeInput:
+		m.setResult(1, m.Input())
+		m.IP += 2
+	case OPCodeOutput:
+		m.Output(m.getParam(1))
+		m.IP += 2
+	case OPCodeJumpIfTrue:
+		if m.getParam(1) > 0 {
+			m.IP = m.getParam(2)
+		} else {
+			m.IP += 3
+		}
+	case OPCodeJumpIfFalse:
+		if m.getParam(1) == 0 {
+			m.IP = m.getParam(2)
+		} else {
+			m.IP += 3
+		}
+	case OPCodeLessThan:
+		v := 0
+		if m.getParam(1) < m.getParam(2) {
+			v = 1
+		}
+		m.setResult(3, v)
+		m.IP += 4
+	case OPCodeEquals:
+		v := 0
+		if m.getParam(1) == m.getParam(2) {
+			v = 1
+		}
+		m.setResult(3, v)
 		m.IP += 4
 	case OPCodeHalt:
 		return false
+	default:
+		panic(fmt.Errorf("Unknown opcode! %d", op))
 	}
 	return true
 }
--- 2019/day05/day.go
+++ 2019/day05/day.go
@@ -1,10 +1,51 @@
 package day05
 
-import "github.com/ags131/adventofcode/2019/aoc"
+import (
+	"fmt"
+
+	"github.com/ags131/adventofcode/2019/aoc"
+	"github.com/ags131/adventofcode/2019/aoc/intcode"
+)
 
 // Run runs this day
 func Run(input *aoc.Input) aoc.Output {
-	part1 := "Not Implemented"
-	part2 := "Not Implemented"
+	var v int
+	vals := make([]int, 0)
+	for {
+		n, _ := fmt.Fscanf(input, "%d", &v)
+		if n == 0 {
+			break
+		}
+		vals = append(vals, v)
+	}
+	clone := func() []int {
+		ret := make([]int, len(vals))
+		for i, v := range vals {
+			ret[i] = v
+		}
+		return ret
+	}
+	part1 := 0
+	part2 := 0
+	m1 := intcode.Machine{
+		Memory: clone(),
+		Input: func() int {
+			return 1
+		},
+		Output: func(v int) {
+			part1 = v
+		},
+	}
+	m1.Run()
+	m2 := intcode.Machine{
+		Memory: clone(),
+		Input: func() int {
+			return 5
+		},
+		Output: func(v int) {
+			part2 = v
+		},
+	}
+	m2.Run()
 	return aoc.Output{Part1: part1, Part2: part2}
 }
