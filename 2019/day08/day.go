67924f02d2ced45ec728588dc828a3bc3391562f
Adam Shumann <ashumann13@gmail.com>
2019-12-07 23:34:43 -0600

    Day8

--- 2019/day08/day.go
+++ 2019/day08/day.go
@@ -1,10 +1,55 @@
 package day08
 
-import "github.com/ags131/adventofcode/2019/aoc"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/ags131/adventofcode/2019/aoc"
+)
 
 // Run runs this day
 func Run(input *aoc.Input) aoc.Output {
-	part1 := "Not Implemented"
-	part2 := "Not Implemented"
+	bytes, _ := ioutil.ReadAll(input)
+	str := string(bytes[:len(bytes)-1])
+	width, height := 25, 6
+	layerLen := width * height
+	layerCnt := len(str) / layerLen
+	layers := make([]map[rune]int, layerCnt)
+	leastZeros := 0
+	for i := 0; i < layerCnt; i++ {
+		cnts := make(map[rune]int, 0)
+		for _, c := range str[i*layerLen : (i+1)*layerLen] {
+			if _, ok := cnts[c]; ok {
+				cnts[c]++
+			} else {
+				cnts[c] = 1
+			}
+		}
+		layers[i] = cnts
+		if cnts['0'] < layers[leastZeros]['0'] {
+			leastZeros = i
+		}
+	}
+	img := make([]rune, layerLen)
+	for i := layerCnt - 1; i >= 0; i-- {
+		layer := str[i*layerLen : (i+1)*layerLen]
+		for j, c := range layer {
+			if c == '2' {
+				continue
+			}
+			img[j] = c
+		}
+	}
+	part1 := layers[leastZeros]['1'] * layers[leastZeros]['2']
+	part2 := "\n"
+	i := 0
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			clr := 40 + (7 * (img[i] - 48))
+			part2 += fmt.Sprintf("\x1b[%dm ", clr)
+			i++
+		}
+		part2 += fmt.Sprint("\x1b[0m\n")
+	}
 	return aoc.Output{Part1: part1, Part2: part2}
 }
